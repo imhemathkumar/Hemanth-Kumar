de26fbf16c0c0ed3968cb8fabd0055dffb0eda6a
Hemanth Kumar Telukuntla <hemanthkumartelukuntla143@gmail.com>
2025-05-20 20:02:50 +0530

    Update route.ts

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,7 +1,7 @@
 import { Resend } from "resend"
 import { NextResponse } from "next/server"
 
-const resend = new Resend(process.env.RESEND_API_KEY)
+const resend = new Resend("re_KaeExhmG_M4XnuSGccKa7u4BPrhanv7Bo")
 
 export async function POST(req: Request) {
   try {
